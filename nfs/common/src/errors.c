5a85af6db806a1c5c57942fd0e205a06a1332cb9
JBalwaySUS <anirudhsankar23@gmail.com>
2024-11-30 18:59:35 +0530

    Update error

--- nfs/common/src/errors.c
+++ nfs/common/src/errors.c
@@ -77,6 +77,7 @@ char *error_messages[NUM_ERRORS] = {
     "Network interfaces error",
     "Socket name error",
     "Storage server disconnected",
+    "send error",
     "mkdir error",
     "open error",
     "remove error",
